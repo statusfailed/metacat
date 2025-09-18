e572a736909e7f14e813f8ff81178b73ae40a719
statusfailed <paul@statusfailed.com>
2025-09-18 22:51:42 +0100

    remove dead util fn

--- src/util.rs
+++ src/util.rs
@@ -24,20 +24,3 @@ where
 pub fn iter_to_array<T, const N: usize>(iter: impl Iterator<Item = T>) -> Option<[T; N]> {
     iter.collect::<Vec<T>>().try_into().ok()
 }
-
-pub fn replace_nodes_in_hypergraph<T, U, V>(
-    term: OpenHypergraph<T, U>,
-    new_nodes: Vec<V>,
-) -> OpenHypergraph<V, U> {
-    use open_hypergraphs::lax::Hypergraph;
-    OpenHypergraph {
-        hypergraph: Hypergraph {
-            nodes: new_nodes,
-            edges: term.hypergraph.edges,
-            adjacency: term.hypergraph.adjacency,
-            quotient: term.hypergraph.quotient,
-        },
-        sources: term.sources,
-        targets: term.targets,
-    }
-}
