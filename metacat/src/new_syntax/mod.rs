efe4fa149d87e80b4713ed566e33b23176f7225d
statusfailed <paul@statusfailed.com>
2026-05-03 07:13:20 +0100

    usage example for new syntax loading

--- metacat/src/new_syntax/mod.rs
+++ metacat/src/new_syntax/mod.rs
@@ -9,6 +9,35 @@
 //! (theory fol.proof fol.syntax { ... })
 //! ```
 //!
+//! A complete file can be loaded directly from a string:
+//!
+//! ```rust
+//! use metacat::new_syntax::{File, Theory, TheoryId};
+//!
+//! let file = File::from_text(
+//!     r#"
+//!     (theory fol.syntax nat {
+//!       (arr wff : 1 -> 1)
+//!       (arr -> : 2 -> 1)
+//!       (arr -. : 1 -> 1)
+//!     })
+//!
+//!     (theory fol.proof fol.syntax {
+//!       (arr wn : wff -> (-. wff))
+//!       (arr wi : {wff wff} -> (-> wff))
+//!       (def win : {wff wff} -> (-> -. wff) = (wi wn))
+//!     })
+//!     "#,
+//! )?;
+//!
+//! let syntax_id = TheoryId("fol.syntax".parse()?);
+//! let proof_id = TheoryId("fol.proof".parse()?);
+//!
+//! assert!(matches!(file.theories.get(&syntax_id), Some(Theory::Theory { .. })));
+//! assert!(matches!(file.theories.get(&proof_id), Some(Theory::Theory { .. })));
+//! # Ok::<(), Box<dyn std::error::Error>>(())
+//! ```
+//!
 //! The implementation is split into:
 //! - [`ast`], which parses raw hexprs into a file-level AST;
 //! - [`nat`], which defines the builtin `nat` syntax category;
